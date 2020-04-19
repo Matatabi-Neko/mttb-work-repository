7ed6cc126e27af0158be50f02679a89f8c439340
Matatabi-Neko <matatabi.animal.neko@gmail.com>
2020-04-19 14:27:02 +0900

    testtest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WorkComponent } from './work/work.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule
